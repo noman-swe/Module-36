e30ce50424f8b5b7dbaf2395f4ff4e534194f459
noman-swe <noman.swe130@gmail.com>
2022-09-19 00:55:48 +0600

    36-4 Loop through an object using for in, for of, object entries

---
+++ 4-loop-compare.js
@@ -0,0 +1,29 @@
+const bottle = {
+    color: 'yellow',
+    held: 'water',
+    price: 50,
+    isCleaned: true
+};
+/* 
+for(let i = 0; i < 10; i++){}
+for(const num of numbers){} //array
+for(const num in numbers){} //object
+*/
+
+for(const property in bottle){
+    console.log(property, bottle[property]);
+}
+
+
+// 
+const keys = Object.keys(bottle);
+
+for(const property of keys){
+    console.log(property, bottle[property]);
+}
+
+// advance
+const entries = Object.entries(bottle);
+for(const [key, value] of Object.entries(bottle)){
+    console.log(key, value);
+}
