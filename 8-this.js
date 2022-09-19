4d99b7c9033b84322868755f6360a4a131531314
noman-swe <noman.swe130@gmail.com>
2022-09-19 23:59:07 +0600

    36-8 ( super advanced) Understand this keyword in JavaScript

---
+++ 8-this.js
@@ -0,0 +1,8 @@
+console.log(this);
+
+/* 
+# this is a execution context
+# jodi regular method hoy taile left side e j asy seta context hobe 
+# jodi arrow method hoy taile uporer level onujayee context hobe 
+# jodi dom er element er event ta kono event handler hoy tahole function ta calling er upor 'This' nirvor korbe 
+*/
